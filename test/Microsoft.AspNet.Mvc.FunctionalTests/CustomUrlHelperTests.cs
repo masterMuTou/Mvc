409940240d99c86ee32a5ec2b3c591d53a99810e
ryanbrandenburg <rybrande@microsoft.com>
2015-10-22 15:49:27 -0700

    * Adding test

--- test/Microsoft.AspNet.Mvc.FunctionalTests/CustomUrlHelperTests.cs
+++ test/Microsoft.AspNet.Mvc.FunctionalTests/CustomUrlHelperTests.cs
@@ -1,9 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Infrastructure;
+using Microsoft.AspNet.Mvc.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Xunit;
 
 namespace Microsoft.AspNet.Mvc.FunctionalTests
@@ -25,6 +30,37 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
         }
 
         public HttpClient Client { get; }
+
+        [Fact]
+        public async Task CustomUrlHelper_UseAllRouteValues()
+        {
+            var result = await Client.GetAsync("/api/RouteValueUsage/urlHelper/helper");
+            var path = await result.Content.ReadAsStringAsync();
+
+            Assert.NotNull(path);
+            Assert.Equal("/api/routevalueusage/get/1234", path);
+        }
+
+        private static IUrlHelper CreateUrlHelper(IServiceProvider services)
+        {
+            var actionContextAccessor = services.GetRequiredService<IActionContextAccessor>();
+            actionContextAccessor.ActionContext = new ActionContext();
+
+            return services.GetRequiredService<IUrlHelper>();
+        }
+
+        private static IServiceCollection GetServices()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<IUrlHelper, UrlHelper>();
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            services.AddTransient<IActionSelector, DefaultActionSelector>();
+            services.AddSingleton<IActionDescriptorsCollectionProvider, DefaultActionDescriptorsCollectionProvider>();
+            services.AddSingleton<IActionSelectorDecisionTreeProvider, ActionSelectorDecisionTreeProvider>();
+            services.AddTransient<ILoggerFactory, LoggerFactory>();
+
+            return services;
+        }
 
         [Fact]
         public async Task CustomUrlHelper_GeneratesUrlFromController()
---
+++ test/WebSites/UrlHelperWebSite/Controllers/RouteValueUsageController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace UrlHelperWebSite.Controllers
+{
+    [Route("api/[controller]")]
+    public class RouteValueUsageController
+    {
+        private readonly IUrlHelper _urlHelper;
+
+        public RouteValueUsageController(IUrlHelper urlHelper)
+        {
+            _urlHelper = urlHelper;
+        }
+
+        [HttpGet("urlHelper/helper")]
+        public string UrlHelper()
+        {
+            return _urlHelper.Action("Get", "RouteValueUsage", new { id = 1234 });
+        }
+
+        [HttpGet("Get/{id}")]
+        public string Get(int id)
+        {
+            return "routevalue: " + id;
+        }
+
+        [HttpGet("Get")]
+        public string Get()
+        {
+            return "BASEGET";
+        }
+    }
+}
